124bfc593f803fcc12a6e451e3ebae8bbbc10690
rztiwari <rztiwari@gmail.com>
2017-05-14 12:20:34 +0100

    Added a add new slide

--- client/container/slideList.js
+++ client/container/slideList.js
@@ -6,6 +6,7 @@ class SlideList extends Component {
   constructor(props){
     super(props);
     this.updateCurrentSlide = this.updateCurrentSlide.bind(this);
+    this.addNewSlide = this.addNewSlide.bind(this);
   }
   componentWillMount() {
     this.props.fetchSlideList();
@@ -13,17 +14,34 @@ class SlideList extends Component {
   updateCurrentSlide(evt){
     this.props.currentSlide(evt.target.innerText);
   }
-  renderList() {
-    if (this.props.slides && this.props.slides.data && this.props.slides.data.length > 0) {
-      return this.props.slides.data.map((slide) => {
+  addNewSlide(){
+    console.log('Add a slide');
+  }
+
+  buildSildeList(slides){
+    return slides.map((slide) => {
+      if(slide !== ''){
         return (
           <li key={slide} className="list-group-item">
             <a href="javascript:void(0);" onClick={this.updateCurrentSlide}>{slide}</a>
           </li>
         );
-      });
-    }
-    return <li></li>;
+      }else{
+        return(
+          <li key={"slide7"} className="list-group-item">
+            <a href="javascript:void(0);" onClick={this.addNewSlide}>Add Slide</a>
+          </li>
+        )
+      }
+    });
+  }
+
+  renderList() {
+    let template,
+    slides = this.props.slides && this.props.slides.data && this.props.slides.data.length > 0 ? this.props.slides.data : [];
+    slides.push('');
+    template = this.buildSildeList(slides);
+    return template;
   }
 
   render() {
